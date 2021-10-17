fa5269daae9d31d4fbed4f4ff0c9d209cbd58efd
maarufvazifdar <maarufvazifdar@gmail.com>
2021-10-17 01:06:27 -0400

    Task:2.8 - Functional implementation and expansion of stub to create InverseKinematics class and its member functions

--- app/inverse_kinematics.cpp
+++ app/inverse_kinematics.cpp
@@ -19,8 +19,57 @@
 #include<sensor.hpp>
 #include<robot.hpp>
 
+Robot robot2(4, 2, 0.3, 0.01, 0.01, 0, 0, 1, 0.785);
+ForwardKinematics fk;
+
 void InverseKinematics::calculateWheelAngles(double new_heading) {
+  if (new_heading < 0) {
+    double left_angle = -atan(robot2.getWheelBase() / (fk.getR1() +
+    robot2.getTrackWidth() / 2));
+    double right_angle = atan(robot2.getWheelBase() / (fk.getR1() -
+    robot2.getTrackWidth() / 2));
+
+    if (left_angle < -robot2.getMaxSteerAngle() && right_angle < -
+    robot2.getMaxSteerAngle()) {
+      robot2.setLeftWheelAng(-robot2.getMaxSteerAngle());
+      robot2.setRightWheelAng(-robot2.getMaxSteerAngle());
+    } else {
+      robot2.setLeftWheelAng(left_angle);
+      robot2.setRightWheelAng(right_angle);
+    }
+  } else if (new_heading > 0) {
+    double left_angle = atan(robot2.getWheelBase() / (fk.getR1() +
+    robot2.getTrackWidth() / 2));
+    double right_angle = -atan(robot2.getWheelBase() / (fk.getR1() -
+    robot2.getTrackWidth() / 2));
+
+    if (left_angle > robot2.getMaxSteerAngle() && right_angle
+    > robot2.getMaxSteerAngle()) {
+      robot2.setLeftWheelAng(robot2.getMaxSteerAngle());
+      robot2.setRightWheelAng(robot2.getMaxSteerAngle());
+    } else {
+      robot2.setLeftWheelAng(left_angle);
+      robot2.setRightWheelAng(right_angle);
+    }
+  } else {
+    double x = 0;
+    robot2.setLeftWheelAng(x);
+    robot2.setRightWheelAng(x);
+  }
+
+  std::cout << "Left Wheel Angle= " << robot2.getLeftWheelAng()
+  << std::endl;
+  std::cout << "Right Wheel Angle= " << robot2.getRightWheelAng()
+  << std::endl;
 }
 
 void InverseKinematics::calculateWheelSpeeds(double new_speed) {
+  robot2.setLeftWheelVel(new_speed / robot2.getWheelRadius());
+  robot2.setRightWheelVel(new_speed / robot2.getWheelRadius());
+
+  std::cout << "Robot Speed= " << new_speed <<std::endl;
+  std::cout << "Left Wheel Speed= " << robot2.getLeftWheelVel()
+  << std::endl;
+  std::cout << "Right Wheel Speed= " << robot2.getRightWheelVel()
+  << std::endl;
 }
