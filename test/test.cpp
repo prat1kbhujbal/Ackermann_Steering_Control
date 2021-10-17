a3f7409e0725f5d6a9c1a56e7c4171c7d27cd0c2
maitreya98 <maitreya kulkarni@maitreyakr@gmail.com>
2021-10-17 00:52:16 -0400

    Task:1.15 - Unit test was created for members of Sensor class

--- test/test.cpp
+++ test/test.cpp
@@ -1,8 +1,12 @@
 #include <gtest/gtest.h>
 #include <iostream>
 #include "../include/pid.hpp"
+#include "../include/controller.hpp"
+#include "../include/robot.hpp"
+#include "../include/sensor.hpp"
+#include "../include/forward_kinematics.hpp"
 
-TEST(computepidTest, should_pass) {
+TEST(computePidTest, gain_update_check) {
     PID pid(0.8, 0.5, 0.3, 1.0, -20, 20);
     double something = pid.compute(0, 5);
     std::cout<<something<<std::endl;
@@ -12,4 +16,75 @@ TEST(computepidTest, should_pass) {
     EXPECT_EQ(0.3, pid.getKd());
     EXPECT_EQ(1.0,pid.getDt());
 }
+/**
+* @brief Test for invalid sampling time
+*/
+TEST(invaliddt, checkParameters) {
+  PID pid1(1, 1, 1, 0, 0, 0);
+  EXPECT_THROW(pid1.compute(1, 5), std::domain_error);
+}
 
+/**
+* @brief Test for invalid PID gain parameters
+*/
+TEST(invalidGains, checkParameters) {
+  // invalid Kp
+  PID pid2(0, 1, 1, 1, 10, 10);
+  EXPECT_THROW(pid2.compute(1, 5), std::domain_error);
+
+  // invalid Ki
+  PID pid3(1, 0, 1, 1, 10, 10);
+  EXPECT_THROW(pid3.compute(1, 5), std::domain_error);
+
+  // invalid Kd
+  PID pid4(1, 1, 0, 1, 10, 10);
+  EXPECT_THROW(pid4.compute(1, 5), std::domain_error);
+}
+
+
+TEST(goalsetterTest, controllerTest) {
+    Controller controller;
+    controller.setGoal(0.5, 10);
+    EXPECT_EQ(10,controller.getGoalSpeed());
+    EXPECT_EQ(0.5,controller.getGoalHeading());
+}
+
+TEST(robotTest, robotMembersTest) {
+    Robot robot(2, 3, 0.2, 10, 10, 0.25, 0.26, 1, 0.785);
+    robot.setLeftWheelVel(25);
+    robot.setRightWheelVel(25);
+    robot.setLeftWheelAng(0.7);
+    robot.setRightWheelAng(0.7);
+    EXPECT_EQ(2, robot.getWheelBase());
+    EXPECT_EQ(3, robot.getTrackWidth());
+    EXPECT_EQ(0.2, robot.getWheelRadius());
+    EXPECT_EQ(25, robot.getLeftWheelVel());
+    EXPECT_EQ(25, robot.getRightWheelVel());
+    EXPECT_EQ(0.7, robot.getLeftWheelAng());
+    EXPECT_EQ(0.7, robot.getRightWheelAng());
+    EXPECT_EQ(1, robot.getComOffset());
+    EXPECT_EQ(0.785, robot.getMaxSteerAngle());
+}
+
+TEST(sensorTest, sensorMemberTest){
+    Sensor sensor;
+    sensor.setCurrerntHeading(0.5);
+    sensor.setCurrerntSpeed(5);
+    EXPECT_EQ(sensor.getCurrerntHeading(), 0.5);
+    EXPECT_EQ(sensor.getCurrentSpeed(), 5);
+}
+
+TEST(forwardKinematicsTest, forwardKinematicsMemeberTest) {
+    ForwardKinematics fk;
+    PID pid_steer(1, 1, 1, 0.1, -0.785, 0.785);
+    PID pid_speed(1, 1, 1, 0.1, 0, 1);
+    Sensor sensor1;
+    sensor1.setCurrerntHeading(0.1);
+    sensor1.setCurrerntSpeed(10);
+    Robot robot1(4, 2, 0.3, 0.1, 0.1, 0, 0, 1, 0.785);
+    fk.setDTheta(0.1);
+    EXPECT_EQ(0,fk.getR1());
+    EXPECT_NEAR(fk.calculateNewHeading(0.1), 0.1, 0.1);
+    EXPECT_NEAR(fk.calculateNewSpeed(5), 5, 0.1);
+    EXPECT_TRUE(fk.goalReached(0.1, 10));
+}
