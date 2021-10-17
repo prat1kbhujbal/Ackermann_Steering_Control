622c66fdf78063f12c37ba916015895a05e6418c
maarufvazifdar <maarufvazifdar@gmail.com>
2021-10-16 22:09:30 -0400

    Task:2.1 - Functional implementation and expansion of stub to create Robot class

--- app/robot.cpp
+++ app/robot.cpp
@@ -12,28 +12,44 @@
 #include <robot.hpp>
 
 double Robot::getWheelBase() {
+  return _wheel_base;
 }
+
 double Robot::getTrackWidth() {
+  return _track_width;
 }
+
 double Robot::getWheelRadius() {
+  return _wheel_radius;
 }
 double Robot::getLeftWheelVel() {
+  return _left_wheel_vel;
 }
 double Robot::getRightWheelVel() {
+  return _right_wheel_vel;
 }
 double Robot::getLeftWheelAng() {
+  return _left_wheel_ang;
 }
 double Robot::getRightWheelAng() {
+  return _right_wheel_ang;
 }
 void Robot::setLeftWheelVel(double left_wheel_vel) {
+  _left_wheel_vel = left_wheel_vel;
 }
 void Robot::setRightWheelVel(double right_wheel_vel) {
+  _right_wheel_vel = right_wheel_vel;
 }
 void Robot::setLeftWheelAng(double left_wheel_ang) {
+  _left_wheel_ang = left_wheel_ang;
 }
 void Robot::setRightWheelAng(double right_wheel_ang) {
+  _right_wheel_ang = right_wheel_ang;
 }
+
 double Robot::getComOffset() {
+  return _com_offset;
 }
 double Robot::getMaxSteerAngle() {
+  return _max_steer_angle;
 }
--- include/robot.hpp
+++ include/robot.hpp
@@ -31,7 +31,16 @@ class Robot {
   Robot(double wheel_base, double track_width, double wheel_radius,
     double left_wheel_vel, double right_wheel_vel,
     double left_wheel_ang, double right_wheel_ang,
-    double com_offset, double max_steer_angle) {}
+    double com_offset, double max_steer_angle)
+      : _wheel_base(wheel_base),
+        _track_width(track_width),
+        _wheel_radius(wheel_radius),
+        _left_wheel_vel(left_wheel_vel),
+        _right_wheel_vel(right_wheel_vel),
+        _left_wheel_ang(left_wheel_ang),
+        _right_wheel_ang(right_wheel_ang),
+        _com_offset(com_offset),
+        _max_steer_angle(max_steer_angle) {}
 
   /**
    * @brief Destructor for class Robot
