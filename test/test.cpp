8ca39e725f780e2af43df17a838862d1cbd72c13
prat33k-dev <bhujbalpratik97@gmail.com>
2021-10-24 00:00:48 -0400

    [Task 2.10] Added and implemented of test cases

--- test/test.cpp
+++ test/test.cpp
@@ -6,50 +6,54 @@
 #include "../include/sensor.hpp"
 #include "../include/forward_kinematics.hpp"
 #include "../include/inverse_kinematics.hpp"
-TEST(computePidTest, gain_update_check) {
+TEST(computePidTest, gain_update_check)
+{
     PID pid(0.8, 0.5, 0.3, 1.0, -20, 20);
     double something = pid.compute(0, 5);
-    std::cout<<something<<std::endl;
+    std::cout << something << std::endl;
     EXPECT_NEAR(something, 8, 0.5);
-    EXPECT_EQ(0.8,pid.getKp());
+    EXPECT_EQ(0.8, pid.getKp());
     EXPECT_EQ(0.5, pid.getKi());
     EXPECT_EQ(0.3, pid.getKd());
-    EXPECT_EQ(1.0,pid.getDt());
+    EXPECT_EQ(1.0, pid.getDt());
 }
 /**
 * @brief Test for invalid sampling time
 */
-TEST(invaliddt, checkParameters) {
-  PID pid1(1, 1, 1, 0, 0, 0);
-  EXPECT_THROW(pid1.compute(1, 5), std::domain_error);
+TEST(invaliddt, checkParameters)
+{
+    PID pid1(1, 1, 1, 0, 0, 0);
+    EXPECT_THROW(pid1.compute(1, 5), std::domain_error);
 }
 
 /**
 * @brief Test for invalid PID gain parameters
 */
-TEST(invalidGains, checkParameters) {
-  // invalid Kp
-  PID pid2(0, 1, 1, 1, 10, 10);
-  EXPECT_THROW(pid2.compute(1, 5), std::domain_error);
+TEST(invalidGains, checkParameters)
+{
+    // invalid Kp
+    PID pid2(0, 1, 1, 1, 10, 10);
+    EXPECT_THROW(pid2.compute(1, 5), std::domain_error);
 
-  // invalid Ki
-  PID pid3(1, 0, 1, 1, 10, 10);
-  EXPECT_THROW(pid3.compute(1, 5), std::domain_error);
+    // invalid Ki
+    PID pid3(1, 0, 1, 1, 10, 10);
+    EXPECT_THROW(pid3.compute(1, 5), std::domain_error);
 
-  // invalid Kd
-  PID pid4(1, 1, 0, 1, 10, 10);
-  EXPECT_THROW(pid4.compute(1, 5), std::domain_error);
+    // invalid Kd
+    PID pid4(1, 1, 0, 1, 10, 10);
+    EXPECT_THROW(pid4.compute(1, 5), std::domain_error);
 }
 
-
-TEST(goalsetterTest, controllerTest) {
+TEST(goalsetterTest, controllerTest)
+{
     Controller controller;
     controller.setGoal(0.5, 10);
-    EXPECT_EQ(10,controller.getGoalSpeed());
-    EXPECT_EQ(0.5,controller.getGoalHeading());
+    EXPECT_EQ(10, controller.getGoalSpeed());
+    EXPECT_EQ(0.5, controller.getGoalHeading());
 }
 
-TEST(robotTest, robotMembersTest) {
+TEST(robotTest, robotMembersTest)
+{
     Robot robot(2, 3, 0.2, 10, 10, 0.25, 0.26, 1, 0.785);
     robot.setLeftWheelVel(25);
     robot.setRightWheelVel(25);
@@ -66,7 +70,8 @@ TEST(robotTest, robotMembersTest) {
     EXPECT_EQ(0.785, robot.getMaxSteerAngle());
 }
 
-TEST(sensorTest, sensorMemberTest){
+TEST(sensorTest, sensorMemberTest)
+{
     Sensor sensor;
     sensor.setCurrerntHeading(0.5);
     sensor.setCurrerntSpeed(5);
@@ -74,23 +79,24 @@ TEST(sensorTest, sensorMemberTest){
     EXPECT_EQ(sensor.getCurrentSpeed(), 5);
 }
 
-TEST(InverseKinematicsTest, inverseKinematicsMembersTest) {
+TEST(InverseKinematicsTest, inverseKinematicsMembersTest)
+{
     Robot robot2(4, 2, 0.3, 0.01, 0.01, 0, 0, 1, 0.785);
-    ForwardKinematics fk2;
     InverseKinematics ik1;
     ik1.calculateWheelAngles(0.1);
     ik1.calculateWheelSpeeds(10);
 }
 
-TEST(InverseKinematicsTest1, inverseKinematicsMembersTest) {
+TEST(InverseKinematicsTest1, inverseKinematicsMembersTest)
+{
     Robot robot3(4, 2, 0.3, 0.01, 0.01, 0, 0, 1, 0.785);
-    ForwardKinematics fk3;
     InverseKinematics ik2;
     ik2.calculateWheelAngles(-0.1);
-    ik2.calculateWheelSpeeds(10);   
+    ik2.calculateWheelSpeeds(10);
 }
 
-TEST(forwardKinematicsTest, forwardKinematicsMemeberTest) {
+TEST(forwardKinematicsTest, forwardKinematicsMemeberTest)
+{
     ForwardKinematics fk;
     PID pid_steer(1, 1, 1, 0.1, -0.785, 0.785);
     PID pid_speed(1, 1, 1, 0.1, 0, 1);
@@ -99,13 +105,14 @@ TEST(forwardKinematicsTest, forwardKinematicsMemeberTest) {
     sensor1.setCurrerntSpeed(10);
     Robot robot1(4, 2, 0.3, 0.1, 0.1, 0, 0, 1, 0.785);
     fk.setDTheta(0.1);
-    EXPECT_EQ(0,fk.getR1());
+    EXPECT_EQ(0, fk.getR1());
     EXPECT_NEAR(fk.calculateNewHeading(0.1), 0.785, 0.1);
     EXPECT_NEAR(fk.calculateNewSpeed(5), 1, 0.1);
     EXPECT_TRUE(fk.goalReached(0.1, 10));
 }
 
-TEST(forwardKinematicsTest1, forwardKinematicsMemeberTest) {
+TEST(forwardKinematicsTest1, forwardKinematicsMemeberTest)
+{
     ForwardKinematics fk1;
     PID pid_steer1(1, 1, 1, 0.1, -0.785, 0.785);
     PID pid_speed1(1, 1, 1, 0.1, 0, 1);
@@ -114,9 +121,7 @@ TEST(forwardKinematicsTest1, forwardKinematicsMemeberTest) {
     sensor2.setCurrerntSpeed(10);
     Robot robot2(4, 2, 0.3, 0.1, 0.1, 0, 0, 1, 0.785);
     fk1.setDTheta(0.1);
-    EXPECT_EQ(0,fk1.getR1());
+    EXPECT_EQ(0, fk1.getR1());
     EXPECT_NEAR(fk1.calculateNewHeading(-0.1), -0.785, 0.1);
     EXPECT_NEAR(fk1.calculateNewSpeed(5), 1, 0.1);
 }
-
-
