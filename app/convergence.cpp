ae3555383516f60cbd8ca00627be63a8c53220ba
maitreya98 <maitreya kulkarni@maitreyakr@gmail.com>
2021-10-24 18:01:14 -0400

    Task:2.16 - Functional implementation and expansion of stub to create Convergence class to graphically demonstrate convergence to goals

--- app/convergence.cpp
+++ app/convergence.cpp
@@ -14,6 +14,27 @@
 #include <string.h>
 #include <iostream>
 
-void Convergence::plotConvergence(double goal, std::vector<double> current, std::string file_name){      
+void Convergence::plotConvergence(double goal, std::vector<double> current, std::string file_name) {
+  RGBABitmapImageReference *canvasReference = CreateRGBABitmapImageReference();
+  std::vector<double> y;
+  y = current;
+  std::vector<double> time;
+  double j = 0;
+  for (int i = 0; i < y.size(); i++)
+  {
+    time.push_back(j);
+    j = j + 0.1;
+  }
+
+  DrawScatterPlot(canvasReference, 1260, 720, &time, &y);
+  double x1 = MapXCoordinateAutoSettings(0, canvasReference->image, &time);
+  double y1 = MapYCoordinateAutoSettings(goal, canvasReference->image, &y);
+
+  double x2 = MapXCoordinateAutoSettings(time.back(), canvasReference->image, &time);
+	double y2 = MapYCoordinateAutoSettings(goal, canvasReference->image, &y);
+  DrawLine(canvasReference->image, x1, y1, x2, y2, 2, GetGray(0.3));
+  std::vector<double> *pngdata = ConvertToPNG(canvasReference->image);
+	WriteToFile(pngdata, file_name);
+	DeleteImage(canvasReference->image);
   
 }
