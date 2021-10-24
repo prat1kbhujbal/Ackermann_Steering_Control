0e0f60304d0f3f1843f3ada191846b589d17fea6
maitreya98 <maitreya kulkarni@maitreyakr@gmail.com>
2021-10-24 17:59:46 -0400

    Task:2.14 - Add Doxygen comments and google style formatting for Convergence stubs

--- app/convergence.cpp
+++ app/convergence.cpp
@@ -3,7 +3,7 @@
  * @author Maitreya Kulkarni
  * @author Maaruf Vazifdar
  * @author Pratik Bhujbal
- * @brief Implementation of class to visualize convergence to goal values
+ * @brief Implementation of class to visualize convergence to goal
  * @version 1.1
  * @date 10/19/2021
  * Copyright [2021] ENPM808X group - MT-15
@@ -15,5 +15,5 @@
 #include <iostream>
 
 void Convergence::plotConvergence(double goal, std::vector<double> current, std::string file_name){      
-
+  
 }
--- include/convergence.hpp
+++ include/convergence.hpp
@@ -11,12 +11,28 @@
 
 #pragma once
 
+/**
+ * @brief Class to visualize convergence.
+ */
 class Convergence{
   public:
   
+  /**
+   * @brief Consructor for class Convergence
+   * @param void
+   * @return void
+   */
   Convergence(){}
-
+  /**
+ * @brief Destructor for class Convergence
+ */
   ~Convergence(){}
 
+   /**
+  * @brief Calculate new speed required to reach the goal speed.
+  * @param goal double - robot's goal speed/heading 
+  * @param current double - vector storing robot's current speed/heading
+  * @return void
+  */
   void plotConvergence(double, std::vector<double>, std::string);
 };
