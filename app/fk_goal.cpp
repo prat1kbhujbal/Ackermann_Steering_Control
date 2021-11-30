945ef938b965482c591cfeb3a245c6ee3951b900
prat33k-dev <bhujbalpratik97@gmail.com>
2021-11-30 02:42:24 -0500

    updated cmakelists

---
+++ app/fk_goal.cpp
@@ -0,0 +1,54 @@
+/**
+ * MIT License
+ *
+ * Copyright (c) 2021 Pratik Bhujnbal
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to 
+ * deal in the Software without restriction, including without limitation the  
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or 
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in 
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+ * DEALINGS IN THE SOFTWARE. 
+ * 
+ * @file fk_goal.cpp
+ * @author Pratik Bhujnbal
+ * @brief Implementation file for FkGoal class
+ * @version 1.1
+ * @date 11/29/2021
+ * @copyright  Copyright (c) 2021
+ * 
+ */
+
+#include<iostream>
+#include<cmath>
+#include<forward_kinematics.hpp>
+#include<pid.hpp>
+#include<controller.hpp>
+#include<sensor.hpp>
+#include<robot.hpp>
+#include<convergence.hpp>
+#include<fk_goal.hpp>
+
+Sensor sensor2;
+FkGoal fkg;
+bool FkGoal::goalReached(double goal_heading, double goal_speed) {
+  if (sensor2.getCurrerntHeading() <= goal_heading
+      && sensor2.getCurrerntHeading() <= (goal_heading + 0.05)
+      && sensor2.getCurrentSpeed() <= goal_speed
+      && sensor2.getCurrentSpeed() <= (goal_speed + 0.05)) {
+    fkg.status = true;
+    return true;
+  }
+  return false;
+}
